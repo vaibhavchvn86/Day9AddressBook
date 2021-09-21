4e52a33a74c4d3ce9583a8875dd1a8936788cb1d
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-21 23:32:03 +0530

    Solution File of Address Book

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day9AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Welcome To Address Book Program****");
+            //#region UC1
+            //UC1_AddBook Contact =new UC1_AddBook("Sham","Roy","High Street","Mumbai","Maharashtra", 400001, 9182736455,"sham.roy@email.com");
+            //Console.WriteLine(Contact.toString());
+            //#endregion
+
+            #region UC2
+            UC2_AddBook.AddPerson();
+            #endregion
+        }
+    }
+}
