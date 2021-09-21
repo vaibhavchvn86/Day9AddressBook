1b83f05e697679ee98380358799337d7ad3c283d
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-21 23:31:20 +0530

    UC2 Address Book

---
+++ UC2_AddBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9AddressBook
+{
+    class UC2_AddBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+
+        public static List<UC2_AddBook> ContactDetail = new List<UC2_AddBook>();
+        public static void AddPerson()
+        {
+            UC2_AddBook person = new UC2_AddBook();
+
+            Console.Write("Enter FirstName: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter LastName: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Mobile Number: ");
+            person.MobileNumber = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+           person.Address = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode : ");
+            person.Zipcode = Console.ReadLine();
+
+            Console.Write("Enter Email : ");
+            person.Email = Console.ReadLine();
+
+            //ContactDetail.Add(person);
+
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Mobile Number: " + person.MobileNumber);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.Zipcode);
+            Console.WriteLine("Email : " + person.Email);
+        }
+    }
+}
