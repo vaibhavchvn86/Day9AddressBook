d2c2504a36b841b72e732927aa0091d275c415ad
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-23 22:14:03 +0530

    Delete a Contact in Address Book

--- UC4_AddBook.cs
+++ UC4_AddBook.cs
@@ -6,23 +6,54 @@ namespace Day9AddressBook
 {
     class UC4_AddBook
     {
-        public string FirstName;
-        public string LastName;
-        public string MobileNumber;
-        public string Address;
-        public string State;
-        public string Zipcode;
-        public string Email;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+
         public static List<UC4_AddBook> ContactDetail = new List<UC4_AddBook>();
         public static void AddBook2()
         {
-            int AddB = 100;
+           
             UC4_AddBook person = new UC4_AddBook();
+            Console.Write("Enter FirstName: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter LastName: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Mobile Number: ");
+            person.MobileNumber = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode : ");
+            person.Zipcode = Console.ReadLine();
+
+            Console.Write("Enter Email : ");
+            person.Email = Console.ReadLine();
+
+            ContactDetail.Add(person);
+
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Mobile Number: " + person.MobileNumber);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.Zipcode);
+            Console.WriteLine("Email : " + person.Email);
+            Console.WriteLine("-------------------------------------------");
             Console.Write("Enter Number AddressBook Id to be Created: ");
             int N = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= N; i++)
             {
-                AddB = AddB + i;
 
                 Console.Write("Enter FirstName: ");
                 person.FirstName = Console.ReadLine();
@@ -47,8 +78,6 @@ namespace Day9AddressBook
 
                 ContactDetail.Add(person);
 
-                Console.WriteLine("AddressBook ID= " + AddB);
-
                 Console.WriteLine("First Name: " + person.FirstName);
                 Console.WriteLine("Last Name: " + person.LastName);
                 Console.WriteLine("Mobile Number: " + person.MobileNumber);
@@ -57,21 +86,16 @@ namespace Day9AddressBook
                 Console.WriteLine("ZipCode : " + person.Zipcode);
                 Console.WriteLine("Email : " + person.Email);
                 Console.WriteLine("-------------------------------------------");
-
-
             }
-            //To edit The Entry
-            Console.WriteLine("AddressBook ID To Delete");
-            int ID = Convert.ToInt32(Console.ReadLine());
-            if (ID == AddB)
-            { 
-                Console.WriteLine("Are you sure you want to remove address book? (Y/N)");
-
-                if (Console.ReadKey().Key == ConsoleKey.Y)
-                {
+            ////To Delete The Entry
+            Console.WriteLine("Are you sure you want to Delete Address Book Entry? (Y/N)");
+            string choice = Convert.ToString(Console.ReadLine());
+            switch (choice)
+            {
+                case "Y":
+                    var Edit1 = Console.ReadLine();
+                    var editobj1 = ContactDetail.Find(x => x.FirstName == Edit1);
                     ContactDetail.Remove(person);
-                    Console.WriteLine("AddressBook "+AddB+"  Deleted. Press any key to continue.");
-                    Console.WriteLine("AddressBook ID= " + AddB);
                     Console.WriteLine("First Name: " + person.FirstName);
                     Console.WriteLine("Last Name: " + person.LastName);
                     Console.WriteLine("Mobile Number: " + person.MobileNumber);
@@ -80,9 +104,13 @@ namespace Day9AddressBook
                     Console.WriteLine("ZipCode : " + person.Zipcode);
                     Console.WriteLine("Email : " + person.Email);
                     Console.WriteLine("-------------------------------------------");
-                    Console.ReadKey();
-                }
+                    Console.WriteLine("Entry Deleted");
+                    break;
+                default:
+                    Console.WriteLine("No Entry to Delete");
+                    break;
             }
+            Console.ReadKey();
         }
     }
 }
