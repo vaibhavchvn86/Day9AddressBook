dbdb14157270f9ef8d39d3f6d3591de61f1f1074
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-03 20:27:35 +0530

    [VaibhavChavan]Added:Solution File of Address Book

--- Program.cs
+++ Program.cs
@@ -1,27 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day9AddressBook
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("****Welcome To Address Book Program****");
-            //#region UC1
-            //UC1_AddBook.Contact();
-            //#endregion
 
-            //#region UC2
-            //UC2_AddBook.AddPerson();
-            //#endregion
+            #region UC1
+            UC1_AddBook.Contact();
+            #endregion
 
-            //#region UC3
-            //UC3_AddBook.AddBook1();
-            //#endregion
+            #region UC2
+            UC2_AddBook.AddPerson();
+            #endregion
+
+            #region UC3
+            UC3_AddBook.AddBook1();
+            #endregion
 
             #region UC4
             UC4_AddBook.AddBook2();
             #endregion
+
+            Console.ReadKey();
         }
     }
 }
+
